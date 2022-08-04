10b37fbd6c0c746d4d3ed14162ed509671ce6e6e
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-08-04 20:04:30 +0530

    nodejs introduction

---
+++ 1. Introduction/1.what_is_node_js.js
@@ -0,0 +1,15 @@
+// what is nodejs?
+// prior to node js, javascript cant be used as general purpose programming language,it is limited to what browsers allows it to do
+
+// nodejs is a way to use js on server
+
+// nodejs use chromes v8 engine to use js outside the browser
+
+// node js is not a programming language it is a runtime environment
+
+//runtime env - runtime provide custom functionality so various tools and libraries specific to a environment
+
+//node js runtime provide developer the functionality like
+//modules and libraries for setting up th web server, modules for integrating with local filesystem
+
+//nodejs and v8 doesn't know how to integrate with file system it is c++ functions that do the job
